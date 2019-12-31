c40b97fbc68a8cb6e248ca6cb29305fa9ef19de7
Ugudei <ugudeit@gmail.com>
2019-12-31 12:05:32 -0800

    working on get ajax requests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,4 +1,25 @@
+
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+
+    },
+    dataType: 'json',
+    videoEmbeddable: true,
+   // success: callback,
+    complete: callback
+
+    // error: errorCB || function(error) {
+    //   console.error('chatterbox: Failed to fetch messages', error);
+    // }
+  });
   // TODO
 };
 
